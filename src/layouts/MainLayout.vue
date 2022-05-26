e66763a89bd272e1681978c8be3e94206b4e12b6
IrleySilvestre <irleysilvestre.mba@gmail.com>
2022-05-26 12:00:16 -0300

    add user profile

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,14 +10,48 @@
           dense
           icon="menu"
         >
-          <q-tooltip>Menu</q-tooltip>
+          <q-tooltip class="bg-secondary">Menu</q-tooltip>
         </q-btn>
 
         <q-space />
 
-        <q-btn round flat @click="drawerRight = !drawerRight" class="q-mr-lg">
+        <q-btn round flat class="q-mr-lg">
           <q-icon name="account_circle" size="2rem" />
-          <q-tooltip>Account</q-tooltip>
+          <q-tooltip class="bg-secondary text-bold">Perfil</q-tooltip>
+          <q-menu>
+            <div class="row no-wrap q-pa-md">
+              <div class="column items-center">
+                <div class="text-h6 q-mb-md">Perfil</div>
+
+                <q-avatar size="75px">
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+                </q-avatar>
+
+                <div class="text-subtitle1 q-mt-md q-mb-xs">
+                  Irley Jose Silvestre
+                </div>
+
+                <q-separator inset class="q-mx-lg" />
+
+                <div class="column">
+                  <!-- <q-toggle v-model="mobileData" label="Use Mobile Data" />
+                  <q-toggle v-model="bluetooth" label="Bluetooth" /> -->
+                  <q-separator />
+                </div>
+
+                <q-btn
+                  class="q-mt-md"
+                  color="secondary"
+                  icon="logout"
+                  label="Sair"
+                  push
+                  size="sm"
+                  v-close-popup
+                  :to="{ name: 'signin' }"
+                />
+              </div>
+            </div>
+          </q-menu>
         </q-btn>
       </q-toolbar>
     </q-header>
@@ -42,40 +76,6 @@
         <q-separator />
         <MainMenu v-for="link in menuLinks" :key="link.title" v-bind="link" />
       </q-list>
-    </q-drawer>
-
-    <q-drawer v-model="drawerRight" side="right">
-      <!-- drawer content -->
-      <q-card class="my-card bg-secondary text-teal-1 text-bold" bordered>
-        <q-item>
-          <q-item-section avatar>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-            </q-avatar>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Irley Jose Silvestre</q-item-label>
-            <q-item-label class="text-teal-1 text-bold" caption>
-              irleysilvestre.mba@gmail.com
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-separator />
-
-        <q-item clickable :to="{ name: 'signin' }" exact>
-          <q-item-section class="text-right">
-            <!-- <q-item-label caption>Encerrar</q-item-label> -->
-            <q-item-label caption class="text-teal-1 text-bold"
-              >Sair</q-item-label
-            >
-          </q-item-section>
-          <q-item-section avatar class="q-mr-lg">
-            <q-icon name="logout" />
-          </q-item-section>
-        </q-item>
-      </q-card>
     </q-drawer>
 
     <q-page-container>
@@ -121,8 +121,9 @@ export default defineComponent({
     return {
       menuLinks: MenuList,
       drawerLeft,
-      drawerRight,
       miniState: ref(true),
+      mobileData: ref(true),
+      bluetooth: ref(false),
     };
   },
 });
